26b83c4e3272059ca7cd684d66ae4bf110d09a2f
geonwoo92 <cnu201102054@gmail.com>
2024-05-13 21:49:55 +0900

    240513

--- backend/app/api/titanic/service/titanic_service.py
+++ backend/app/api/titanic/service/titanic_service.py
@@ -3,16 +3,55 @@ from app.api.titanic.model.titanic_model import TitanicModel
 import pandas as pd
 
 
+
+
 class TitanicService:
 
     model = TitanicModel() 
 
     def process(self):
         print(f'프로세스 시작')
-        train_model = self.new_model('train.csv')
-        test_model = self.new_model('test.csv')
-        print(f'트레인 컬럼 : {train_model.columns}')
-        print(f'테스트 컬럼 : {test_model.columns}')
+        this = self.model
+        this.train = self.new_model('train.csv')
+        this.test = self.new_model('test.csv')
+    
+        self.df_info(this)
+        
+        # print(f'트레인 컬럼 : {this.train.columns}')
+        # print(f'테스트 컬럼 : {this.test.columns}')
+        this.id=this.test['PassengerId']
+        self.drop_feature(this,'Ticket','Cabin','SibSp','Parch','Name')
+  
+        self.df_info(this)
+        
+        
+        this = self.create_train(this)
+        
+    @staticmethod
+    def drop_feature(this, *feature) -> object: #f*은 여러개를 받을 수 있다.
+
+        
+        # for i in feature:
+        #     this.train = this.train.drop([i], axis=1)
+        #     this.test = this.test.drop(i, axis=1)
+            
+        # for i in [this.train, this.test]:
+        #    for j in feature:
+        #        i.drop(j, axis=1, inplace=True)
+               
+        [i.drop(j, axis=1, inplace=True) for j in feature for i in [this.train, this.test]] 
+        
+        return this
+        
+        
+    @staticmethod
+    def df_info(this):
+           
+        [print(f'{i}') for i in [this.train, this.test] ] 
+            
+        
+        
+    
 
     def new_model(self, playload) -> object:
         this = self.model
@@ -28,11 +67,7 @@ class TitanicService:
     def create_label(this) -> str:
         return this.train['Survived'] 
 
-    @staticmethod
-    def drop_feature(this, *feature) -> object:
-        for i in feature:
-            pass
-        
+
 
     
     
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import { useForm, SubmitHandler } from "react-hook-form";
 
+
 type Message = {
   content: string;
   type: string;
@@ -9,88 +10,100 @@ type Message = {
 
 type Inputs = {
   question: string;
+  category: string
+  exampleRequired?: string
 }
 
 export default function Home() {
   const [message, setMessage] = useState('')
+  const [category, setCategory] = useState('')
 
-  const { watch,register, handleSubmit, formState: { errors }, reset } = useForm<Inputs>();
+  const { watch, register, handleSubmit, formState: { errors }, reset } = useForm<Inputs>();
 
   const onSubmit: SubmitHandler<Inputs> = (data) => {
     console.log('입력된 값 : ' + JSON.stringify(data));
-    fetch('http://localhost:8000/api/chat/titanic', {
+    fetch('http://localhost:8000/api/chat/'+ `${category}`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
     })
-    .then((response) => response.json()) // 응답을 JSON으로 파싱
-    .then((data) => {
-      console.log("응답:", data); // 응답 데이터를 콘솔에 출력
-      setMessage(data); // 응답 데이터를 상태에 저장
-    })
-    .catch((error) => console.log("에러:", error));
-    
+      .then((response) => response.json()) // 응답을 JSON으로 파싱
+      .then((data) => {
+        console.log("응답:", data); // 응답 데이터를 콘솔에 출력
+        setMessage(data); // 응답 데이터를 상태에 저장
+      })
+      .catch((error) => console.log("에러:", error));
+
+  }
+
+  const handleRadio = (e: any) => {
+    setCategory(e.target.value)
+    console.log("select button : ", e.target.value)
   }
   // console.log(watch("question"))
- 
 
-  
+
+
   return (
-  <>  <div className="h-screen flex justify-center items-center bg-gray-100">
-  <div className="w-full h-full max-w-10xl bg-white rounded-lg shadow-lg">
-    <div className="p-16">
-      <div className="flex items-center justify-center">
-        <h1 className="text-4xl font-bold">geon GPT에게 물어봐</h1>
-      </div><br />
-     
-        <div className="grid grid-cols-3 gap-4 mb-4">
-          <div className="border border-gray-300 p-4 rounded-lg">
-            <label className="inline-flex items-center">
-              <input type="radio" className="form-radio" name="radio" />
-              <span className="ml-2">버튼1</span>
-            </label>
-            {/* 버튼1 아래에 사진이 들어갈 공간 */}
-            <img src="/image/titanic.jpg" alt="Example" className="mt-4 w-full h-15 object-cover rounded-lg" />
+    <>  <div className="h-screen flex justify-center items-center bg-gray-100">
+      <div className="h-screen flex justify-center items-center backgroundImage" //배경 이미지
+        style={{
+          backgroundImage: "url('/image/background.jpg')", // 상대 경로 수정
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <div className="p-8">
+          <div className="flex items-center justify-center">
+            <h1 className="text-4xl font-bold text-white">GEON GPT에게 물어봐</h1>
+          </div><br />
+          <div className="grid grid-cols-3 gap-4 mb-4">
+            <div className="border border-gray-300  p-4 rounded bg-white">
+              <label className="inline-flex items-center">
+                <input onClick={handleRadio} value={"titanic"} type="radio" className="form-radio" name="radio" />
+                <span className="ml-2">Titanic</span>
+              </label>
+              {/* 버튼1 아래에 사진이 들어갈 공간 */}
+              <img src="/image/titanic.jpg" alt="Example" className="mt-4 w-full h-15 object-cover rounded-lg" />
+            </div>
+            <div className="border border-gray-300 p-4 rounded bg-white">
+              <label className="inline-flex items-center">
+                <input onClick={handleRadio} value={"new2"} type="radio" className="form-radio" name="radio" />
+                <span className="ml-2">버튼2</span>
+              </label>
+              {/* 버튼2 아래에 사진이 들어갈 공간 */}
+              <img src="/image/back.jpg" alt="Example" className="mt-4 w-full h-15 object-cover rounded-lg" />
+            </div>
+            <div className="border border-gray-300 p-4 rounded bg-white">
+              <label className="inline-flex items-center">
+                <input onClick={handleRadio} value={"new3"} type="radio" className="form-radio" name="radio" />
+                <span className="ml-2">버튼3</span>
+              </label>
+              <img src="/image/street.jpg" alt="Example" className="mt-4 w-full h-15 object-cover rounded-lg" />
+            </div>
+          </div><br />
+          <div className="border border-gray-300 rounded bg-white p-4 mb-4 h-64 overflow-y-auto text-lg">
 
-
+            <h4>{message ? message : ""}</h4>
           </div>
-          <div className="border border-gray-300 p-4 rounded-lg">
-            <label className="inline-flex items-center">
-              <input type="radio" className="form-radio" name="radio" />
-              <span className="ml-2">버튼2</span>
-            </label>
-            {/* 버튼2 아래에 사진이 들어갈 공간 */}
-            <img src="/image/back.jpg" alt="Example" className="mt-4 w-full h-15 object-cover rounded-lg" />
-          </div>
-          <div className="border border-gray-300 p-4 rounded-lg">
-            <label className="inline-flex items-center">
-              <input type="radio" className="form-radio" name="radio" />
-              <span className="ml-2">버튼3</span>
-            </label>
-            {/* 버튼3 아래에 사진이 들어갈 공간 */}
-          </div>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <input
+              type="text"
+              {...register("question", { required: true })}
+              className="block w-full py-4 px-6 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-blue-500"
+              placeholder="메시지를 입력하세요..."
+            />
+            <button
+              type="submit"
+              className="py-4 px-8 bg-blue-500 text-white font-bold rounded-lg transition duration-300 ease-in-out hover:bg-blue-600"
+            >
+              전송
+            </button>
+          </form>
         </div>
-        <div className="bg-gray-200 rounded-lg p-4 mb-4 h-48 overflow-y-auto text-lg">
-        <h4>{message ? message: "" }</h4>
       </div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input
-          type="text"
-          {...register("question", { required: true })}
-          className="block w-full py-4 px-6 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-blue-500"
-          placeholder="메시지를 입력하세요..."
-        />
-        <button
-          type="submit"
-          className="py-4 px-8 bg-blue-500 text-white font-bold rounded-lg transition duration-300 ease-in-out hover:bg-blue-600"
-        >
-          전송
-        </button>
-      </form>
     </div>
-  </div>
-</div>
-</>)
+    </>)
 }
